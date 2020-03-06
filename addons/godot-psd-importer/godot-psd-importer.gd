1417f4f8eedda700ca268c647a56ee05ea5cd612
2shady4u <jd16581556@gmail.com>
2020-03-06 11:16:38 +0100

    removed present from license

--- addons/godot-psd-importer/godot-psd-importer.gd
+++ addons/godot-psd-importer/godot-psd-importer.gd
@@ -1,7 +1,7 @@
 # ############################################################################ #
-# Copyright © 2020-present Piet Bronders & Jeroen De Geeter <piet.bronders@gmail.com>
+# Copyright © 2020 Piet Bronders & Jeroen De Geeter <piet.bronders@gmail.com>
+# Copyright © 2020 Gamechuck d.o.o. <gamechuckdev@gmail.com>
 # Licensed under the MIT License.
-# See LICENSE in the project root for license information.
 # ############################################################################ #
 
 tool
